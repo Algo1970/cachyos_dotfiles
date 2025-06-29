ec61fe0d71af42e9b95b2d0017f9dca656628644
Shinkotoni_Base <iwant2user@gmail.com>
2025-06-29 13:25:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,28 @@
 ### sudo pacman -S fcitx5 fcitx5-mozc fcitx5-gtk fcitx5-qt
 ### sudo pacman -S fcitx5-configtool
 
-### add `.profile`
-### export GTK_IM_MODULE=fcitx
-### export QT_IM_MODULE=fcitx
-### export XMODIFIER=@im=fcitx
-### export INPUT_METHOD=fcitx
-### export SDL_IM_MODULE=fcitx
-### export GLFW_IM_MODULE=fcitx
+set profile_file ~/.profile
+
+# 追記する内容
+set lines '
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIER=@im=fcitx
+export INPUT_METHOD=fcitx
+export SDL_IM_MODULE=fcitx
+export GLFW_IM_MODULE=fcitx
+'
+
+# 含まれていない場合のみ追記
+if not grep -q 'GTK_IM_MODULE=fcitx' $profile_file
+    echo $lines >> $profile_file
+    echo "✅ Fcitx環境変数を .profile に追記しました"
+else
+    echo "ℹ️ 既に .profile に設定があります（追記しませんでした）"
+end
 
 ### add `.config/hypr/hyprland.conf`
-### exec-once = fcitx5
+grep -q 'exec-once *= *fcitx5' ~/.config/hypr/hyprland.conf; or echo 'exec-once = fcitx5' >> ~/.config/hypr/hyprland.conf
 
 ### set mozc
 ### fcitx5-configtool

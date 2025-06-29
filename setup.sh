39518b02206ef64acbbb11fb6e39d733b48ea63b
Shinkotoni_Base <iwant2user@gmail.com>
2025-06-29 13:52:40 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env fish
 
+### change directory_name
+LANG=C xdg-user-dirs-update --force
+
+### AUR（Arch User Repository）サポートツール
+sudo pacman -S yay
+
+### copy & paste
+sudo pacman -S wl-clipboard
+
+### capslock -> left Ctrl, touchpad scroll
+
+grep -q 'kb_options = ctrl:nocaps' ~/.config/hypr/hyprland.conf; or echo -e '\ninput {\n    kb_options = ctrl:nocaps\n\n    touchpad {\n        natural_scroll = true\n    }\n}' >> ~/.config/hypr/hyprland.conf
+
 ### install mozc
-### sudo pacman -S fcitx5 fcitx5-mozc fcitx5-gtk fcitx5-qt
-### sudo pacman -S fcitx5-configtool
+sudo pacman -S fcitx5 fcitx5-mozc fcitx5-gtk fcitx5-qt
+sudo pacman -S fcitx5-configtool
 
 set profile_file ~/.profile
 
@@ -30,10 +43,14 @@ grep -q 'exec-once *= *fcitx5' ~/.config/hypr/hyprland.conf; or echo 'exec-once 
 ### set mozc
 ### fcitx5-configtool
 
-### 
 
 ### install google-chrome, ranger
 
 sudo pacman -S ranger
-# paru -S google-chrome
+ranger --copy-config all
+sed -i 's/^set show_hidden false$/set show_hidden true/' ~/.config/ranger/rc.conf
+
+paru -S google-chrome
+
+
 

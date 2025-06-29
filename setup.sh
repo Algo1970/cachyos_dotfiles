cb4226de5dacf38544e0c0ac6f6ae1c6cee122cd
Shinkotoni_Base <iwant2user@gmail.com>
2025-06-29 13:14:18 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env fish
 
+### install mozc
+### sudo pacman -S fcitx5 fcitx5-mozc fcitx5-gtk fcitx5-qt
+### sudo pacman -S fcitx5-configtool
 
+### add `.profile`
+### export GTK_IM_MODULE=fcitx
+### export QT_IM_MODULE=fcitx
+### export XMODIFIER=@im=fcitx
+### export INPUT_METHOD=fcitx
+### export SDL_IM_MODULE=fcitx
+### export GLFW_IM_MODULE=fcitx
 
+### add `.config/hypr/hyprland.conf`
+### exec-once = fcitx5
+
+### set mozc
+### fcitx5-configtool
+
+### 
 
 ### install google-chrome, ranger
 

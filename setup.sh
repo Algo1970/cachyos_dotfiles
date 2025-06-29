a1389803f80d8010e78d4623030395ad3e88e3c8
Shinkotoni_Base <iwant2user@gmail.com>
2025-06-29 13:54:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,25 +17,7 @@ grep -q 'kb_options = ctrl:nocaps' ~/.config/hypr/hyprland.conf; or echo -e '\ni
 sudo pacman -S fcitx5 fcitx5-mozc fcitx5-gtk fcitx5-qt
 sudo pacman -S fcitx5-configtool
 
-set profile_file ~/.profile
-
-# 追記する内容
-set lines '
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIER=@im=fcitx
-export INPUT_METHOD=fcitx
-export SDL_IM_MODULE=fcitx
-export GLFW_IM_MODULE=fcitx
-'
-
-# 含まれていない場合のみ追記
-if not grep -q 'GTK_IM_MODULE=fcitx' $profile_file
-    echo $lines >> $profile_file
-    echo "✅ Fcitx環境変数を .profile に追記しました"
-else
-    echo "ℹ️ 既に .profile に設定があります（追記しませんでした）"
-end
+echo -e '\nexport GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\nexport XMODIFIER=@im=fcitx\nexport INPUT_METHOD=fcitx\nexport SDL_IM_MODULE=fcitx\nexport GLFW_IM_MODULE=fcitx' >> ~/.profile
 
 ### add `.config/hypr/hyprland.conf`
 grep -q 'exec-once *= *fcitx5' ~/.config/hypr/hyprland.conf; or echo 'exec-once = fcitx5' >> ~/.config/hypr/hyprland.conf
